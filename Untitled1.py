b6d87bc23c6a1b85f792e5046e63fff606d1c4f4
Alejandra Gallegos <147637085+AlexGallegos8@users.noreply.github.com>
2024-04-02 11:31:56 -0600

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Teoría de conjuntos
+
+# La teoría de conjuntos es una tarea fundamental de las matemáticas que trata sobre las propiedades y relaciones entre conjuntos, que son colecciones bien definidas de objetos. Python, como lenguaje de programación versátil, ofrece varias formas de trabajar con conjuntos y aplicar conceptos de teoría de conjuntos.
+
+# ## Creación de conjuntos
+
+# In[5]:
+
+
+lista = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+
+
+# In[6]:
+
+
+lista
+
+
+# In[9]:
+
+
+conjunto = set(lista)
+
+
+# In[10]:
+
+
+conjunto
+
+
+# ## Operaciones
+
+# Python proporciona operadores y métodos para realizar operaciones básicas de conjuntos como unión, intersección, diferencia y diferencia simétrica
+
+# ## Unión
+
+# La operación unión en teoria de conjuntos es una operacion que combina dos conjuntos para formar un nuevo conjunto que contiene todos los elementos
+
+# $$
+# C= A\cup B = {x:x \in A \quad o \quad x \in B}
+# $$
+# 
+# $$ es LaTeX in a Jupyter Notebook
+
+# In[ ]:
+
+
+A | B
+
+
+# In[ ]:
+
+
+A.union(B)
+
+
+# ## Intersección
+
+# La intersección de conjuntos es una operación que devuelve un nuevo conjunto que contiene únicamente los elementos comunes a dos o más conjuntos. Es decir, se seleccionan aquellos elementos que están presentes en todos los conjuntos involucrados en la operación de intersección.
+# 
+# $$
+# R \cap S = {x|x \in R \land x \in S }
+# $$
+
+# ## Diferencia
+
+# Sean  y  dos conjuntos cualesquiera. El conjunto diferencia de  y , que se representa por , es el conjunto formado por todos los elementos que están en , pero no están en .
+# 
+# Sean  y  dos conjuntos. La diferencia de conjuntos  es:
+# 
+# $$
+# A - B = {x \in A y x \notin B}
+# $$
+
+# ## Diferencia simétrica
+
+# La diferencia simétrica es el conjunto de elementos que solo pertenecen A o B pero no a ambos a la vez.
+# 
+# $$
+# A \Delta B = {x/(x \in A y x \notin B) o (x \in B y x \notin A)}
+# $$
